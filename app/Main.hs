aecc4f6bbd4adc23c1eb25e6d208ac3ba87740a2
Satoshi Ogata <satosystems@gmail.com>
2017-09-02 21:39:41 +0900

    implement normal

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
-module Main where
+import System.Environment (getArgs)
 
-import Lib
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 2) + fib (n - 1)
+
+normal :: IO ()
+normal = do
+  print $ fib 39
+  print $ fib 40
+  print $ fib 41
+  print $ fib 42
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  case args of
+    ("normal":_) -> normal
+    _ -> return ()
+
